fe8a2390b9aa14cd9a41d1d978164361ebf77eaf
PRANAV77-PY <pbse1377@gmail.com>
2025-10-04 13:36:59 +0530

    Add files via upload

---
+++ grid x@y.py
@@ -0,0 +1,31 @@
+#xAxis
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([80,85,90,95,100,105,110,115,120,125])
+y = np.array([240,250,260,270,280,290,300,310,320,330])
+
+plt.title('Sports Data')
+plt.xlabel('Average Pulse')
+plt.ylabel('Calorie Burnage')
+
+plt.grid(axis = 'x')
+plt.plot(x,y)
+
+plt.show()
+
+#Yaxis
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([80,85,90,95,100,105,110,115,120,125])
+y = np.array([240,250,260,270,280,290,300,310,320,330])
+
+plt.title('Sports Data')
+plt.xlabel('Average Pulse')
+plt.ylabel('Calorie Burnage')
+
+plt.grid(axis = 'y')
+plt.plot(x,y)
+
+plt.show()
